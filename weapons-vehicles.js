84fcd724a0d1b340290630025986f5fd2f44ad1d
AIS1697 <f22.raptor.mk2@gmail.com>
2026-02-17 13:55:56 +0900

    Create weapons-vehicles.js

---
+++ weapons-vehicles.js
@@ -0,0 +1,150 @@
+[{
+    "name":"F-16C AIM-9 #1,6",
+    "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "https://OC135.github.io/test6/f16caim916.glb"
+},
+{
+    "name":"Fuel Truck",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "https://OC135.github.io/test6/f16caim925.glb"
+},
+    {
+    "name":"Catering Truck",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/737max_427352_3011/cttruck7.glb"
+},
+     {
+    "name":"Airport bus",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/a3test_427352_3436/aptbus10.glb"
+},
+     {
+    "name":"Luggage car",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Boeing%20p8I%20Neptune_427352_3292/luggagecar.glb"
+},
+    {
+    "name":"Cargo ramp",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/a3test_427352_3436/cargo_ramp8.glb"
+},
+      {
+    "name":"Fire truck",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Boeing%20p8I%20Neptune_427352_3292/firetruck1.glb"
+},
+{
+    "name":"Jetbridge 1",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Airbus a321-211 _427352_5086/jwt11.glb"
+},
+{
+    "name":"Jetbridge 2",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Airbus a321-211 _427352_5086/jwt6.glb"
+},
+{
+    "name":"Jetbridge 3",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Airbus a321-211 _427352_5086/jetwaytruck4.glb"
+},
+{
+    "name":"Jetbridge 4",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Airbus a321-211 _427352_5086/jwt8.glb"
+},
+{
+    "name":"Jetbridge 5",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Boeing 737-600_427352_5203/jwt12.glb"
+},{
+    "name":"Airport Gate (Left)",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/SAAB%20340_427352_2892/gate2.glb"
+},{
+    "name":"Airport Gate (Right)",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/SAAB%20340_427352_2892/gate1.glb"
+},
+{
+    "name":"Airport Gate#2 level 1",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Boeing%20p8I%20Neptune_427352_3292/gate_1.glb"
+},
+{
+    "name":"Airport Gate#2 level 2",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Boeing%20p8I%20Neptune_427352_3292/gate_2.glb"
+},{
+    "name":"Airport Gate#2 level 3",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Boeing%20p8I%20Neptune_427352_3292/gate_3.glb"
+},
+{
+    "name":"Airport Gate#3 level 1",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Bombardier Learjet 45 XR_427352_5073/jb31.glb"
+},
+{
+    "name":"Airport Gate#3 level 2",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Bombardier Learjet 45 XR_427352_5073/jb32.glb"
+},
+    {
+    "name":"Airport Gate#3 level 3",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/Bombardier Learjet 45 XR_427352_5073/jb33.glb"
+},
+{
+    "name":"Airport Gate#4 level 1",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/a3test_427352_3436/gate_5.glb"
+},
+{
+    "name":"Airport Gate#4 level 2",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/a3test_427352_3436/gate_6.glb"
+},
+{
+    "name":"Airport Gate#4 level 3 (a380)",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "../../../../backend/aircraft/repository/a3test_427352_3436/gate_7.glb"
+}]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
