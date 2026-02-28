66503b140ce773dde568ceae970630bfdefc3029
AIS1697 <f22.raptor.mk2@gmail.com>
2026-02-28 18:27:17 +0900

    Update weapons-vehicles.js

--- weapons-vehicles.js
+++ weapons-vehicles.js
@@ -897,7 +897,7 @@
     "name":"F-15J formation flight",
      "rotation":[0,0,0],
     "position":[0,0,0],
-    "model": "https://OC135.github.io/test6/f15j1.glb"
+    "model": "https://OC135.github.io/test6/f15jff.glb"
 },
 {
     "name":"Airport Gate#4 level 2",
