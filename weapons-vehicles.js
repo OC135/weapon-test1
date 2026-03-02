e830d047b508989507e0174428732d4325715859
AIS1697 <f22.raptor.mk2@gmail.com>
2026-03-02 14:10:04 +0900

    Update weapons-vehicles.js

--- weapons-vehicles.js
+++ weapons-vehicles.js
@@ -654,6 +654,12 @@
     "model": "https://OC135.github.io/test6/mh601.glb"
 },
 {
+    "name":"MH-60R US.NAVY",
+     "rotation":[0,0,0],
+    "position":[0,0,0],
+    "model": "https://OC135.github.io/test6/mh60r1.glb"
+},
+{
     "name":"KC-767 boom off",
      "rotation":[0,0,0],
     "position":[0,0,0],
@@ -892,12 +898,6 @@
      "rotation":[0,0,0],
     "position":[0,0,0],
     "model": "https://OC135.github.io/test6/nimitz2.glb"
-},
-{
-    "name":"F-15J formation flight",
-     "rotation":[0,0,0],
-    "position":[0,0,0],
-    "model": "https://OC135.github.io/test6/f15jff1.glb"
 },
 {
     "name":"Airport Gate#4 level 2",
